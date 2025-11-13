7d201f5e10053add8725da8dc428f79734552cd6
JohnLloydProg <johnlloydunida0@gmail.com>
2025-11-14 04:03:34 +0800

    finished game mechanic

--- snake.py
+++ snake.py
@@ -1,10 +1,21 @@
 import pygame
 import os
 
-STRAIGHT = pygame.image.load("./assets/straight.png")
-CORNER = pygame.image.load("./assets/corner.png")
-TAIL = pygame.image.load("./assets/tail.png")
-HEAD = pygame.image.load("./assets/head.png")
+cell_size = os.environ.get("CELL_SIZE", "50")
+VERTICAL = pygame.transform.scale(pygame.image.load("./assets/straight.png"), (int(cell_size), int(cell_size)))
+HORIZONTAL = pygame.transform.rotozoom(VERTICAL, 90, 1.0)
+DOWN_RIGHT = pygame.transform.scale(pygame.image.load("./assets/corner.png"), (int(cell_size), int(cell_size)))
+RIGHT_UP = pygame.transform.rotozoom(DOWN_RIGHT, 90, 1.0)
+LEFT_UP = pygame.transform.rotozoom(DOWN_RIGHT, 180, 1.0)
+LEFT_DOWN = pygame.transform.rotozoom(DOWN_RIGHT, 270, 1.0)
+DOWN_TAIL = pygame.transform.scale(pygame.image.load("./assets/tail.png"), (int(cell_size), int(cell_size)))
+RIGHT_TAIL = pygame.transform.rotozoom(DOWN_TAIL, 90, 1.0)
+UP_TAIL = pygame.transform.rotozoom(DOWN_TAIL, 180, 1.0)
+LEFT_TAIL = pygame.transform.rotozoom(DOWN_TAIL, 270, 1.0)
+UP_HEAD = pygame.transform.scale(pygame.image.load("./assets/head.png"), (int(cell_size), int(cell_size)))
+LEFT_HEAD = pygame.transform.rotozoom(UP_HEAD, 90, 1.0)
+DOWN_HEAD = pygame.transform.rotozoom(UP_HEAD, 180, 1.0)
+RIGHT_HEAD = pygame.transform.rotozoom(UP_HEAD, 270, 1.0)
 
 class Snake:
     def __init__(self,  map:list, tail:tuple[int, int]=(0, 0), head:tuple[int, int]=(0, 2),):
@@ -30,17 +41,29 @@ class Snake:
         if event.type == pygame.KEYDOWN:
             next_step = self.get_next_step()
             if event.key == pygame.K_UP and next_step[pygame.K_UP] not in self.body and self.map[next_step[pygame.K_UP][1]][next_step[pygame.K_UP][0]] != 'x':
-                self.body.append((self.body[-1][0], self.body[-1][1] - 1))
-                self.body.pop(0)
+                self.up()
             if event.key == pygame.K_DOWN and next_step[pygame.K_DOWN] not in self.body and self.map[next_step[pygame.K_DOWN][1]][next_step[pygame.K_DOWN][0]] != 'x':
-                self.body.append((self.body[-1][0], self.body[-1][1] + 1))
-                self.body.pop(0)
+                self.down()
             if event.key == pygame.K_LEFT and next_step[pygame.K_LEFT] not in self.body and self.map[next_step[pygame.K_LEFT][1]][next_step[pygame.K_LEFT][0]] != 'x':
-                self.body.append((self.body[-1][0] - 1, self.body[-1][1]))
-                self.body.pop(0)
+                self.left()
             if event.key == pygame.K_RIGHT and next_step[pygame.K_RIGHT] not in self.body and self.map[next_step[pygame.K_RIGHT][1]][next_step[pygame.K_RIGHT][0]] != 'x':
-                self.body.append((self.body[-1][0] + 1, self.body[-1][1]))
-                self.body.pop(0)
+                self.right()
+    
+    def up(self):
+        self.body.append((self.body[-1][0], self.body[-1][1] - 1))
+        self.body.pop(0)
+    
+    def down(self):
+        self.body.append((self.body[-1][0], self.body[-1][1] + 1))
+        self.body.pop(0)
+    
+    def left(self):
+        self.body.append((self.body[-1][0] - 1, self.body[-1][1]))
+        self.body.pop(0)
+    
+    def right(self):
+        self.body.append((self.body[-1][0] + 1, self.body[-1][1]))
+        self.body.pop(0)
     
     def sprite_for_cell(self, body:int):
         _prev, _body, _next = self.body[body-1], self.body[body], self.body[body+1]
@@ -49,29 +72,59 @@ class Snake:
         dx_next, dy_next = _next[0] - _body[0], _next[1] - _body[1]
 
         if (dx_prev * dx_next, dy_prev * dy_next) in [(1, 0), (-1, 0)]:
-            return pygame.transform.rotozoom(STRAIGHT, 90, 1.0)
+            return HORIZONTAL
         elif (dx_prev * dx_next, dy_prev * dy_next) in [(0, 1), (0, -1)]:
-            return STRAIGHT
+            return VERTICAL
         elif (dx_prev, dy_prev) == (0, -1) and (dx_next, dy_next) == (-1, 0) or (dx_next, dy_next) == (0, 1) and (dx_prev, dy_prev) == (1, 0):
-            return (255, 0, 0) # down-left or left-down
+            return LEFT_DOWN # down-left or left-down
         elif (dx_prev, dy_prev) == (1, 0) and (dx_next, dy_next) == (0, -1) or (dx_next, dy_next) == (-1, 0) and (dx_prev, dy_prev) == (0, 1):
-            return (0, 255, 0) # right-down or down-right
+            return LEFT_UP # right-down or down-right
         elif (dx_prev, dy_prev) == (-1, 0) and (dx_next, dy_next) == (0, 1) or (dx_next, dy_next) == (1, 0) and (dx_prev, dy_prev) == (0, -1):
-            return (0, 0, 255) # right-down or down-right
+            return DOWN_RIGHT # left-up or up-left
         else:
-            return (0, 0, 0) # right-up
+            return RIGHT_UP # right-up or up-right
     
     def head_for_cell(self):
+        prev = self.body[-2]
         head = self.body[-1]
+        dx, dy = head[0] - prev[0], head[1] - prev[1]
+        
+
+        if dx > 0:
+            return RIGHT_HEAD
+        elif dx < 0:
+            return LEFT_HEAD
+        elif dy > 0:
+            return DOWN_HEAD
+        else:
+            return UP_HEAD
+    
+    def tail_for_cell(self):
+        body = self.body[1]
+        tail = self.body[0]
+        dx, dy = body[0] - tail[0], body[1] - tail[1]
+        
+
+        if dx > 0:
+            return LEFT_TAIL
+        elif dx < 0:
+            return RIGHT_TAIL
+        elif dy > 0:
+            return UP_TAIL
+        else:
+            return DOWN_TAIL
         
     
     def draw(self, window:pygame.Surface):
         head = self.body[-1]
         tail = self.body[0]
 
-        pygame.draw.rect(window, (0, 255, 0), (tail[0]*self.cell_size, tail[1]*self.cell_size, self.cell_size, self.cell_size))
+        tail_image = self.tail_for_cell()
+        window.blit(tail_image, tail_image.get_rect(topleft=(tail[0]*self.cell_size, tail[1]*self.cell_size)))
         for i in range(1, len(self.body)-1):
             part = self.body[i]
-            pygame.draw.rect(window, self.sprite_for_cell(i), (part[0]*self.cell_size, part[1]*self.cell_size, self.cell_size, self.cell_size))
-        pygame.draw.rect(window, (0, 200, 0), (head[0]*self.cell_size, head[1]*self.cell_size, self.cell_size, self.cell_size))
+            body_image= self.sprite_for_cell(i)
+            window.blit(body_image, body_image.get_rect(topleft=(part[0]*self.cell_size, part[1]*self.cell_size)))
+        head_image = self.head_for_cell()
+        window.blit(head_image, head_image.get_rect(topleft=(head[0]*self.cell_size, head[1]*self.cell_size)))
 
--- ui.py
+++ ui.py
@@ -1,27 +1,144 @@
 import pygame
+from typing import Callable
+
+FONT = pygame.font.Font(None, 25)
+SHELL_EVENT = pygame.USEREVENT + 1
 
 
 class Terminal:
     text = ""
+    history:list[str] = []
     focused = False
+    shell_mode = False
+    y_offset = 0
+    text_bottom = 0
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
-        self.font = pygame.font.Font(None, 25)
+    
+    def change_mode(self, state):
+        self.shell_mode = state
+        self.history.clear()
+        self.text = ""
 
     def handle(self, event:pygame.event.Event):
         if (event.type == pygame.MOUSEBUTTONDOWN):
             self.focused = self.rect.collidepoint(event.pos)
+        elif (event.type == pygame.MOUSEWHEEL and self.focused):
+            print(self.y_offset, self.text_bottom, event.y)
+            if (event.y > 0 and self.y_offset < 0 or event.y < 0 and self.text_bottom > self.rect.height - 10):
+                self.y_offset += (event.y * 10)
         if (event.type == pygame.KEYDOWN and self.focused):
             if (event.key == pygame.K_BACKSPACE and len(self.text) > 0):
                 self.text = self.text[:len(self.text)-1]
+            elif (event.key in [pygame.K_RETURN, pygame.K_KP_ENTER]):
+                if (self.shell_mode):
+                    self.history.append(self.text)
+                    pygame.event.post(pygame.event.Event(SHELL_EVENT, {"instruction":self.text}))
+                    self.text = ""
+                    return
+                else:
+                    self.text += "\n"
             else:
                 self.text += event.unicode
-        
     
     def draw(self, window:pygame.Surface):
         pygame.draw.rect(window, (255, 255, 255), self.rect)
+        if (self.shell_mode):
+            self.text_bottom = self.rect.top + 10
+            for i, line in enumerate(self.history):
+                text = FONT.render(line, False, (0, 0, 0))
+                rect = text.get_rect(topleft=(self.rect.left + 10, self.y_offset + (i*text.get_height()) + self.rect.top + 10))
+                window.blit(text, rect)
+                if i == len(self.history)-1:
+                    self.text_bottom = rect.bottom
+            shell_text = FONT.render(f">>>{self.text}", False, (0, 0, 0))
+            rect = shell_text.get_rect(topleft=(self.rect.left + 10, self.text_bottom))
+            self.text_bottom += rect.height
+            window.blit(shell_text, rect)
+        else:
+            for i, line in enumerate(self.text.split("\n")):
+                text = FONT.render(f"{i+1}: {line}", False, (0, 0, 0))
+                rect = text.get_rect(topleft=(self.rect.left + 10, self.y_offset + (i*text.get_height()) + self.rect.top + 10))
+                window.blit(text, rect)
+                if i == len(self.text.split("\n"))-1:
+                    self.text_bottom = rect.bottom
         pygame.draw.rect(window, (120, 120, 120), self.rect, 10)
-        text = self.font.render(self.text, False, (0, 0, 0))
-        window.blit(text, (self.rect.left + 10, self.rect.top + 10))
         
+
+class ButtonBehavior:
+    def __init__(self, left:float, top:float, width:float, height:float, on_press:Callable|None, args:tuple=()):
+        self.rect = pygame.Rect(left, top, width, height)
+        self.clickable:bool = True
+        self.args = args
+        self.on_press = on_press
+        self.inside = False
+    
+    def clicked(self, event:pygame.event.Event, consumed:list) -> bool:
+        mouse = pygame.mouse.get_pos()
+        if (event.type == pygame.MOUSEMOTION and self.rect.collidepoint(mouse) and self.clickable):
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+            self.inside = True
+        elif (self.inside and not self.rect.collidepoint(mouse)):
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+            self.inside = False
+        if (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and event not in consumed):
+            if (self.rect.collidepoint(mouse) and self.clickable):
+                if (self.on_press):
+                    if (self.args):
+                        self.on_press(*self.args)
+                    else:
+                        self.on_press()
+                consumed.append(event)
+                return True
+        return False
+
+
+class TextButton(ButtonBehavior):
+    def __init__(self, left: float, top: float, width: float, height: float, on_press: Callable, background_color:tuple[int, int, int], content:str, args:tuple=()):
+        super().__init__(left, top, width, height, on_press, args)
+        self.surface = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.surface.fill((0, 0, 0, 100))
+        self.background_color = background_color
+        self.content = content
+    
+    def get_text(self) -> tuple[pygame.Surface, pygame.Rect]:
+        text = FONT.render(self.content, True, (0, 0, 0))
+        return (text, text.get_rect(center=self.rect.center))
+    
+    def draw(self, window:pygame.Surface) -> None:
+        pygame.draw.rect(window, self.background_color, self.rect)
+        if (not self.clickable):
+            window.blit(self.surface, self.rect)
+        if (self.content):
+            text, rect = self.get_text()
+            window.blit(text, rect)
+
+
+class ToggleButton(ButtonBehavior):
+    def __init__(self, left:float, top:float, width:float, height:float, on_toggle:Callable, background_color:tuple[int, int, int], content:tuple[str, str]):
+        super().__init__(left, top, width, height, None)
+        self.surface = pygame.Surface((width, height), pygame.SRCALPHA)
+        self.surface.fill((0, 0, 0, 80))
+        self.background_color = background_color
+        self.on_toggle = on_toggle
+        self.content = content
+        self.down = False
+    
+    def get_text(self) -> tuple[pygame.Surface, pygame.Rect]:
+        text = FONT.render(self.content[0], True, (0, 0, 0)) if not self.down else FONT.render(self.content[1], True, (0, 0, 0))
+        return (text, text.get_rect(center=self.rect.center))
+    
+    def clicked(self, event:pygame.event.Event, consumed:list) -> bool:
+        if (super().clicked(event, consumed)):
+            self.down = not self.down
+            if (self.on_toggle):
+                self.on_toggle(self)
+            return True
+        return False
+    
+    def draw(self, window:pygame.Surface) -> None:
+        pygame.draw.rect(window, self.background_color, self.rect)
+        if (self.content):
+            text, rect = self.get_text()
+            window.blit(text, rect)
